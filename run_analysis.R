b48396632b1f58edc4dbd641109dd429f8c5af16
peppetrick <giuseppe.tricarico@gmail.com>
2015-09-24 08:49:37 +0200

    final version of script

--- run_analysis.R
+++ run_analysis.R
@@ -2,15 +2,15 @@ library(plyr)
 library(reshape2)
 # 1)read data common to train and test data set: feature and activity
 #read feaure.txt
-features<-read.table("features.txt",sep=" ")
+features<-read.table("features.txt")
 names(features)<-c("Code","Label")
 #read activity label
-activit_labels<-read.table("activity_labels.txt",sep=" ")
+activit_labels<-read.table("activity_labels.txt")
 names(activit_labels)<-c("Code","Label")
 
 #2) read test set
 #read subject test
-subject_test<-read.table("./test/subject_test.txt",sep=" ")
+subject_test<-read.table("./test/subject_test.txt")
 
 #read activity class for test set 
 activity_test<-read.table("./test/y_test.txt")
@@ -21,7 +21,7 @@ test_set<-read.table("./test/X_test.txt")
 #3)read train set
 
 #read subject test
-subject_train<-read.table("./train/subject_train.txt",sep=" ")
+subject_train<-read.table("./train/subject_train.txt")
 
 
 #read activity class for test set 
@@ -30,7 +30,7 @@ activity_train<-read.table("./train/y_train.txt")
 train_set<-read.table("./train/X_train.txt")
 
 
-#bind train and test set
+#4)bind train and test set
 total_set<-rbind(train_set,test_set)
 #add name to columns
 names(total_set)<-features$Label;
@@ -45,8 +45,8 @@ total_mean_std$activity<-activity_total
 total_mean_std$subject<-subject_total
 #Uses descriptive activity names to name the activities in the data set
 total_mean_std_activity<-merge(total_mean_std,activit_labels,by.x="activity",by.y="Code")
-#obtain a tydy data set 
+#5)obtain a tidy data set 
 total_melt<-melt(total_mean_std_activity,id=c("Label","subject"),measure.vars=colnames(total_mean_std))
-#result data set
+#12)result data set
 result_data_set<-ddply(total_melt,c("Label","subject","variable"),summarize,mean=mean(value))
 
